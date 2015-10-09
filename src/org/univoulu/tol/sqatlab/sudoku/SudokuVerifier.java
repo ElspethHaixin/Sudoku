21c1fff7e27301f06ee69b7e752a742621cc485d
somename <someemail>
2015-10-09 22:09:21 +0800

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,11 +2,56 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 
-	public String sub;
-	public int[] helper;
+	private String sub;
+	private int[] helper;
 
 	public int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
+		if (candidateSolution.length() != 81)
+			return 1;
+
+		for (int i = 0; i < candidateSolution.length(); i++) {
+			if (candidateSolution.charAt(i) > '9' || candidateSolution.charAt(i) <
+
+			'1')
+				return -1;
+		}
+
+		for (int i = 0, j = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				sub = new String();
+				sub += candidateSolution.substring(i * 3 * 9 + j * 3, i * 3 * 9
+
+				+ j * 3 + 3);
+				sub += candidateSolution.substring((i * 3 + 1) * 9 + j * 3, (i *
+
+				3 + 1) * 9 + j * 3 + 3);
+				sub += candidateSolution.substring((i * 3 + 2) * 9 + j * 3, (i *
+
+				3 + 2) * 9 + j * 3 + 3);
+				if (check9() == false)
+					return -2;
+			}
+		}
+
+		for (int i = 0, j = 0; i < 9; i++) {
+			sub = new String();
+			for (j = 0; j < 9; j++) {
+				sub += candidateSolution.charAt(i * 9 + j);
+			}
+			if (check9() == false)
+				return -3;
+		}
+
+		for (int i = 0, j = 0; j < 9; j++) {
+			sub = new String();
+			for (i = 0; i < 9; i++) {
+				sub += candidateSolution.charAt(i * 9 + j);
+			}
+			if (check9() == false)
+				return -4;
+		}
+
 		return 0;
 	}
 
@@ -23,4 +68,5 @@ public class SudokuVerifier {
 		}
 		return true;
 	}
+	
 }
