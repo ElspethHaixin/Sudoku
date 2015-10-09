be667436b56afae5309950ac7764bf6805dce690
somename <someemail>
2015-10-09 22:19:07 +0800

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -18,7 +18,7 @@ public class TestSudokuVerifier {
 	public void testVerify() {
 		SudokuVerifier sv = new SudokuVerifier();
 		int exp1 = 0;
-		boolean exp2 = false;
+		int exp2 = -2;
 		assertEquals(exp1, sv.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
 		assertEquals(exp2, sv.verify("123456789912345678891234567789123456678912345567891234456789123345678912234567891"));
 	}
